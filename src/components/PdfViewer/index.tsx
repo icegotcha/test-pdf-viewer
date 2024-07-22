3252927b555badcf7fd33f7f50e09db8dd3d9a6b
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2024-07-22 22:23:26 +0700

    chore: Refactor PdfViewer component to improve responsiveness and remove unused code

--- src/components/PdfViewer/index.tsx
+++ src/components/PdfViewer/index.tsx
@@ -13,46 +13,19 @@ interface IPdfViewerProps {
 export default function PdfViewer(props: IPdfViewerProps) {
   const { fileUrl } = props
   const { isMobile, isTablet } = useMatchMedia()
-  const containerRef = useRef<HTMLDivElement>(null)
 
   const [numPages, setNumPages] = useState(0)
-  const [pageWidth, setPageWidth] = useState<number | undefined>(undefined)
   const scale = isMobile ? 0.9 : isTablet ? 1.2 : 1.5
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages)
   }
 
-  useEffect(() => {
-    if (!containerRef.current?.parentElement) {
-      return
-    }
-    const handleResize = () => {
-      setPageWidth(containerRef?.current?.parentElement?.offsetWidth)
-    }
-    if (scale <= 0.9) {
-      window.addEventListener('resize', handleResize)
-      handleResize()
-    } else {
-      window.removeEventListener('resize', handleResize)
-      setPageWidth(undefined)
-    }
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [scale])
-
   return (
-    <Container ref={containerRef}>
+    <Container>
       <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages), (_, index) => (
-          <Page
-            key={`page_${index + 1}`}
-            pageNumber={index + 1}
-            renderAnnotationLayer={false}
-            scale={scale}
-            width={pageWidth}
-          />
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} renderAnnotationLayer={false} scale={scale} />
         ))}
       </Document>
     </Container>
--- src/components/PdfViewer/style.ts
+++ src/components/PdfViewer/style.ts
@@ -5,8 +5,19 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   overflow: hidden auto;
+  background-color: white;
   & > div {
     width: 100%;
     height: 100%;
+  }
+
+  @media (max-width: 768px) {
+    .react-pdf__Page {
+      width: 100% !important;
+      canvas {
+        width: 100% !important;
+        height: 100% !important;
+      }
+    }
   }
 `
